27646b9ced0f6560b4c3474c857c381bb1d0cf44
unknown <toledopabloesteban@gmail.com>
2020-03-20 22:01:43 -0300

    login features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Login from './components/authCliente/Login';
+import AuthClienteState from './context/authCliente/authClienteState';
 
 function App() {
   return (
-    <div className="App">       
-        <Login/>                    
+    <div className="App">
+        <AuthClienteState>
+          <Login/>            
+        </AuthClienteState>               
     </div>
   );
 }
---
+++ src/context/types/index.js
@@ -0,0 +1,5 @@
+export const START_SESION = 'START_SESION';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const SHOW_ALERT = 'SHOW_ALERT';
+export const GET_USER = 'GET_USER';
