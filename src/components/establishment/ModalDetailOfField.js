6b5a8eabc1749033dd701c24c604f8237e4392b8
unknown <toledopabloesteban@gmail.com>
2020-04-19 19:40:20 -0300

    some fixes

--- src/components/establishment/ModalDetailOfField.js
+++ src/components/establishment/ModalDetailOfField.js
@@ -42,7 +42,7 @@ const ModalDetailOfField = () => {
                         </div>
 
                         <div className="form-row">
-                            <label for="inputSportType" 
+                            <label htmlFor="inputSportType" 
                                    className="form-group col-md-4"
                             >Tipo de deporte: </label>
                             <div className="form-group col-md-7">
@@ -54,7 +54,7 @@ const ModalDetailOfField = () => {
                                 />
                             </div>
                             
-                            <label for="inputGroundType" 
+                            <label htmlFor="inputGroundType" 
                                 className="form-group col-md-4"
                             >Tipo de suelo: </label>
                             <div className="form-group col-md-7">
@@ -62,7 +62,7 @@ const ModalDetailOfField = () => {
                                         className="form-control-edited" 
                                         id="inputGroundType" 
                                         readOnly 
-                                        value={selected_field ? selected_field.ground_type.description : '' }
+                                        value={selected_field ? selected_field.ground_type.description : '' }                                        
                                 />
                             </div>                            
                         </div>
@@ -73,32 +73,17 @@ const ModalDetailOfField = () => {
                                 <label className="modal_check_label">Cancha Techada</label>
                             </div>
 
-                            <div className="custom-control custom-radio custom-control-inline">
-                                <input type="radio" 
+                            <div className="custom-control custom-checkbox custom-control-inline">
+                                <input type="checkbox" 
                                         className="custom-control-input" 
                                         id="roofedYes" 
-                                        name="roofedYes" 
-                                        disabled="true"
+                                        name="roofedYes"
+                                        readOnly                                         
                                         checked={selected_field ? 
-                                                        selected_field.is_roofed ? "checked" : ''
-                                                          : '' }
+                                            selected_field.is_roofed  : false}                                                            
                                 />
-                                <label className="custom-control-label" for="roofedYes">SI</label>
-                            </div>
-
-                               
-                            <div className="custom-control custom-radio custom-control-inline">
-                                <input type="radio" 
-                                        className="custom-control-input" 
-                                        id="roofedNo" 
-                                        name="roofedNo" 
-                                        disabled="true"
-                                        checked={selected_field ? 
-                                                        !selected_field.is_roofed ? "checked" : ''
-                                                        : '' }
-                                />
-                                <label className="custom-control-label" for="roofedNo">NO</label>
-                            </div>
+                                <label className="custom-control-label" htmlFor="roofedYes"></label>                                
+                            </div>                                                        
                         </div>
                          
                         <div className="form-row modal_check"> 
@@ -106,38 +91,23 @@ const ModalDetailOfField = () => {
                                 <label className="modal_check_label_short">Iluminada</label>
                             </div>
 
-                            <div className="custom-control custom-radio custom-control-inline">
+                            <div className="custom-control custom-checkbox custom-control-inline">
                                 <input type="radio" 
                                         className="custom-control-input" 
                                         id="lightingYes" 
-                                        name="lightingYes" 
-                                        disabled="true"
+                                        name="lightingYes"
+                                        readOnly                                         
                                         checked={selected_field ? 
-                                            selected_field.has_lighting ? "checked" : ''
-                                              : '' }                                        
-                            />
-                                <label className="custom-control-label" for="lightingYes">SI</label>
-                            </div>
-
-                               
-                            <div className="custom-control custom-radio custom-control-inline">
-                                <input type="radio" 
-                                        className="custom-control-input" 
-                                        id="lightingNo" 
-                                        name="lightingNo" 
-                                        disabled="true" 
-                                        checked={selected_field ? 
-                                            !selected_field.has_lighting ? "checked" : ''
-                                              : '' }      
-                                />
-                                <label className="custom-control-label" for="lightingNo">NO</label>
+                                            selected_field.has_lighting : false}                                        
+                                /> 
+                                <label className="custom-control-label" htmlFor="lightingYes"></label>                              
                             </div>
                         </div>
                         
                         
                         <div className="form-row">
-                            <label for="inputNumberPlayers" className="form-group col-md-4">Cantidad de Jugadores:</label>
-                            <div class="form-group col-md-4">
+                            <label htmlFor="inputNumberPlayers" className="form-group col-md-4">Cantidad de Jugadores:</label>
+                            <div className="form-group col-md-4">
                                 <input type="text" 
                                         className="form-control-edited" 
                                         id="inputNumberPlayers" 
@@ -148,7 +118,7 @@ const ModalDetailOfField = () => {
                                                     
                         </div>
                         <div className="form-row"> 
-                            <label for="inputPrice" className="form-group col-md-4">Precio: </label>
+                            <label htmlFor="inputPrice" className="form-group col-md-4">Precio: </label>
                             <div className="form-group col-md-4">
                                 <input type="text" 
                                         className="form-control-edited" 
@@ -166,7 +136,7 @@ const ModalDetailOfField = () => {
                     <button type="button" 
                             className="btn" 
                             data-dismiss="modal"
-                    >Close</button>
+                    >Cerrar</button>
                 </div>
                 </div>
             </div>
