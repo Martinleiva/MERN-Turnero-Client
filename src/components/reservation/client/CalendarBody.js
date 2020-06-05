ae62dc6ec0e4ba5dbaaaf2cdc616f81f1ced580a
martin <martin_leiva13@hotmail.com>
2020-06-05 11:40:33 -0300

    Implementation of sweet alert with reservation description

--- src/components/reservation/client/CalendarBody.js
+++ src/components/reservation/client/CalendarBody.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import * as dates from 'date-arithmetic';
+import Swal from 'sweetalert2';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import reservationContext from '../../../context/reservations/reservationContext';
@@ -21,7 +23,7 @@ const CalendarBody = () => {
     } = reservationsContext;
 
     const establishmentsContext = useContext(establishmentContext);
-    const { selected_field } = establishmentsContext;
+    const { selected_field, selected_stablishment } = establishmentsContext;
 
     const authContext = useContext(AuthContext);
     const { user } = authContext;
@@ -33,35 +35,90 @@ const CalendarBody = () => {
 
     const handleSelect = ({ start, end }) => {
 
-        console.log(moment(Date.now()).format('LL'));
-        console.log(localizer);
-
         if (start > moment(Date.now()).toDate()) {
-            const nombre = window.prompt('Su nombre: ');
-            if (nombre) {
-                reservation.field_id = selected_field._id;
-                setReservation({
-                    ...reservation,
-                    start,
-                    end,
-                    title: nombre,
-                    field_id: selected_field._id, // id de la cancha actual
-                    user_id: user._id //id del usuario actual
-                });
-                addReservation({
-                    ...reservation,
-                    start,
-                    end,
-                    title: nombre,
-                    field_id: selected_field._id, // id de la cancha actual
-                    user_id: user._id //id del usuario actual
-                });
-            }
 
-            //Obtain reservations 
-            getReservationsByField(selected_field._id);
+            let hourCount = dates.diff(start, end, "hours", true);
+
+            Swal.fire({
+                title: '¿Confirmar reserva?',
+                html: `
+                    <div class="text-left pl-5 pt-4" style="border: 1px solid #3085d6">
+                        <p>
+                            <b>Fecha:</b> ${moment(start).format('LL')}    
+                        </p>
+                        <p>
+                            <b>Horario del turno:</b> De ${moment(start).format('LT')} a ${moment(end).format('LT')} <b style="color: #10850e">(${hourCount} hs)</b>
+                        </p>
+                        <p>
+                            <b>A nombre de:</b> 
+                            <span style="color: #3085d6">
+                                <b>${user.names}</b>
+                            <span>
+                        </p>
+                        <p>
+                            <b>Complejo:</b> 
+                            <span style="color: #10850e">
+                                <b>${selected_stablishment.name}</b>
+                            <span>
+                        </p>
+                        <p>
+                            <b>Cancha:</b>
+                            <span style="color: #10850e">
+                                <b>${selected_field.name}</b>
+                            <span>
+                        </p>
+                    </div>
+                `,
+                icon: 'info',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí, confirmar!',
+                cancelButtonText: "Cancelar"
+            }).then((result) => {
+
+
+                if (result.value) {
+                    Swal.fire(
+                        'Reservado!',
+                        'Tu reserva fué registrada en estado PENDIENTE!',
+                        'success'
+                    )
+
+                    reservation.field_id = selected_field._id;
+
+                    setReservation({
+                        ...reservation,
+                        start,
+                        end,
+                        title: user.names,
+                        field_id: selected_field._id, // id de la cancha actual
+                        user_id: user._id //id del usuario actual
+                    });
+
+                    addReservation({
+                        ...reservation,
+                        start,
+                        end,
+                        title: user.names,
+                        field_id: selected_field._id, // id de la cancha actual
+                        user_id: user._id //id del usuario actual
+                    });
+
+                    //Obtain reservations 
+                    getReservationsByField(selected_field._id);
+                } else {
+                    return;
+                }
+            })
+
         } else {
-            window.alert('No se puede reservar antes de la hora actual');
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'No puedes reservar antes de la hora actual!',
+            })
+
             return;
         }
     }
